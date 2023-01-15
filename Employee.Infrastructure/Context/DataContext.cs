01bde4ea89d839c947fe9acd2d9e4f4d9225995d
Rovid Kashyap <rvd.kashyap@hotmail.com>
2023-01-15 19:15:04 +0530

    Update DataContext.cs

--- Employee.Infrastructure/Context/DataContext.cs
+++ Employee.Infrastructure/Context/DataContext.cs
@@ -17,10 +17,10 @@ namespace Employee.Infrastructure.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Core.Entities.Employee>().HasData(
-                new Core.Entities.Employee { EmployeeId = 1, Department = "HR", FirstName = "Rovid", LastName = "Kashyap", Email = "rvd.kashyap@gmail.com", Mobile = "9760828296" },
-                new Core.Entities.Employee { EmployeeId = 2, Department = "IT", FirstName = "Andrew", LastName = "James", Email = "andrew.james@mail.com", Mobile = "8909371977" },
-                new Core.Entities.Employee { EmployeeId = 3, Department = "Administration", FirstName = "Ferry", LastName = "Synes", Email = "ferry.synes@mail.com", Mobile = "9045326030" },
-                new Core.Entities.Employee { EmployeeId = 4, Department = "HR", FirstName = "Methew", LastName = "Berry", Email = "methew.berry@mail.com", Mobile = "9456201030" });   
+                new Core.Entities.Employee { EmployeeId = 1, Department = "HR", FirstName = "Rovid", LastName = "Kashyap", Email = "rvd.kashyap@gmail.com", Mobile = "9760000000" },
+                new Core.Entities.Employee { EmployeeId = 2, Department = "IT", FirstName = "Andrew", LastName = "James", Email = "andrew.james@mail.com", Mobile = "8001000000" },
+                new Core.Entities.Employee { EmployeeId = 3, Department = "Administration", FirstName = "Ferry", LastName = "Synes", Email = "ferry.synes@mail.com", Mobile = "9111100001" },
+                new Core.Entities.Employee { EmployeeId = 4, Department = "HR", FirstName = "Methew", LastName = "Berry", Email = "methew.berry@mail.com", Mobile = "9000110000" });   
 
             base.OnModelCreating(modelBuilder);
         }
